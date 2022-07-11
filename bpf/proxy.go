c17bd90d55f75f8fb6efbd5ac07fca8c441dca63
gwuah <griffithawuah15@gmail.com>
2022-07-11 17:31:51 +0000

    use new generated iobjects

--- bpf/proxy.go
+++ bpf/proxy.go
@@ -2,25 +2,24 @@ package bpf
 
 import (
 	"net"
-	"C"
 	"github.com/cilium/ebpf/link"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf ../proxy.c
 
-func LoadBPFObjectsIntoKernel() (*proxyObjects, error) {
-	objs := proxyObjects{}
-	err := loadProxyObjects(&objs, nil)
+func LoadBPFObjectsIntoKernel() (*bpfObjects, error) {
+	objs := bpfObjects{}
+	err := loadBpfObjects(&objs, nil)
 	return &objs, err
 }
 
-func AttachToHookpoint(o *proxyObjects, i *net.Interface) (link.Link, error) {
+func AttachToHookpoint(o *bpfObjects, i *net.Interface) (link.Link, error) {
 	return link.AttachXDP(link.XDPOptions{
 		Program:   o.ReverseProxy,
 		Interface: i.Index,
 	})
 }
 
-func SetCurrentPort(obj *proxyObjects, port int) error {
-	return obj.proxyMaps.Ports.Put(uint32(0), uint64(port))
+func SetCurrentPort(obj *bpfObjects, port int) error {
+	return obj.bpfMaps.Ports.Put(uint32(0), uint64(port))
 }
